d15a735a112ea04cd0d0104c2daffc0882dacc60
Ratthapol Sakolchaiphraisan <ratthapol.sk@tt-ss.net>
2023-08-04 10:37:11 +0700

    fix: button component is working incorrect

--- src/libs/Button/Button.tsx
+++ src/libs/Button/Button.tsx
@@ -1,14 +1,9 @@
 import React, { ButtonHTMLAttributes } from "react";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
-
-const Button: React.FC = (props: ButtonProps) => {
-  const  {
-    ...rest
-  } = props;
-  return <button {...rest} />;
+const Button: React.FC<ButtonProps> = ({ ...restProps }) => {
+  return <button {...restProps} />;
 };
-
 Button.displayName = "Button";
 export { Button };
 export type { ButtonProps };
